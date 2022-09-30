d4d2729e51fb812b8159c997c71bba3c5eaa4e4e
David <david.castro001@edu.pucrs.br>
2022-09-30 18:53:55 -0300

    Fixed: agora os exames de um paciente não podem ser realizadas por outro paciente

--- src/Entidades/Paciente.java
+++ src/Entidades/Paciente.java
@@ -10,10 +10,11 @@ public class Paciente extends Usuario {
         super(nome, tipoUsuario);
     }
 
-    public void marcarExameRealizado(Date data, Exame exame){
+    public void marcarExameRealizado(Date data, Exame exame) {
         Date examemais30 = exame.getDataRealizada();
         examemais30.setTime(examemais30.getTime() + 30L * 24 * 60 * 60 * 1000);
-        if(exame.getDataRealizada().before(exame.getDataCadastro()) ){
+        if(exame.getPaciente().getId() != this.getId()){System.out.print("O Exame solicitado não é deste paciente");}
+        else if(exame.getDataRealizada().before(exame.getDataCadastro()) ){
             System.out.println("Data de exame inválida pois é menor que a data de sua autorização!");
         }else if(exame.getDataRealizada().after(examemais30)){ System.out.println("A data de exame é posterior à 30 dias desde sua autorização, por isso ele não pode ser realizado!");}
         else{exame.setRealizado(true); exame.setDataRealizada(data);}
