240d3ff5a149a5fa8186f4bed5659fca4cd0799a
Leonardo Bertinatto <105060693+LBertinatto@users.noreply.github.com>
2022-10-06 21:41:33 -0300

    Filtro função 3

--- src/Entidades/Autorizacao.java
+++ src/Entidades/Autorizacao.java
@@ -2,11 +2,15 @@ package src.Entidades;
 
 import java.util.ArrayList;
 import java.util.Collections;
+
+import src.Enums.TipoExame;
+
 import java.util.*;
 public class Autorizacao {
 
     private ArrayList<Exame> exames;
-    private List<Paciente> filtroNome;
+    private List<Exame> filtroNome;
+    private List<Exame> filtroTipoExame;
 
     public Autorizacao() {
         exames = new ArrayList<>();
@@ -23,12 +27,31 @@ public class Autorizacao {
         return new ArrayList<>(exames);
     }
    
-    public Paciente filtroPaciente(Paciente paciente){
-        for (int i = 0; i < exames.size(); i++) {
-            filtroNome = new ArrayList<>();
-            filtroNome.add(exames.get(i).getPaciente());
+    static class SortByDate implements Comparator<Exame> {
+        @Override
+        public int compare(Exame a, Exame b) {
+            return a.getDataCadastro().compareTo(b.getDataCadastro());
         }
-        System.out.println(filtroNome.toString()); 
-        return paciente;
+    }
+    
+    public List<Exame> filtroPaciente(Paciente paciente){
+        filtroNome = new ArrayList<>();
+        for (int i = 0; i < exames.size(); i++) {
+            if(exames.get(i).getPaciente().equals(paciente))
+            filtroNome.add(exames.get(i));
+        }
+        Collections.sort(filtroNome, new SortByDate());
+        return filtroNome;
+    }
+
+    public List<Exame> filtroTipoExames(TipoExame tipoExame){
+        filtroTipoExame = new ArrayList<>();
+        for (int i = 0; i < exames.size(); i++) {
+            if(exames.get(i).getTipoExame().equals(tipoExame))
+            filtroTipoExame.add(exames.get(i));
+        }
+        Collections.sort(filtroTipoExame, new SortByDate());
+        return filtroTipoExame;
     }
 }
+
